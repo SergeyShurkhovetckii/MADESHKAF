7d4321fa102b7c05d890ec2d0df74da78f4c9e1a
S19S93 <S19S93@yandex.ru>
2021-12-30 17:16:36 +0200

    Master

--- src/blocks/modules/front-materials/front-materials.js
+++ src/blocks/modules/front-materials/front-materials.js
@@ -1,31 +1,32 @@
-(function toggleTabs () {
-	const priceBtn = document.querySelectorAll('.price-top-btn');
-	const tab = document.querySelectorAll('.price-bottom');
-	const select = document.querySelector('.price-top__mob-nav');
+(function toggleTabs() {
+    const materialsBtn = document.querySelectorAll('.materials__btn');
+    const tab = document.querySelectorAll('.materials-tab');
+    const select = document.querySelector('.price-top__mob-nav');
 
-	if (priceBtn && tab && (priceBtn.length == tab.length)) {
-		for (let i = 0; i < priceBtn.length; i++) {
-			priceBtn[i].addEventListener('click', function(e) {
-				e.preventDefault();
-				changeActive(i);
-			})
-		}
-	}
+    if (materialsBtn && tab && (materialsBtn.length == tab.length)) {
+        for (let i = 0; i < materialsBtn.length; i++) {
+            materialsBtn[i].addEventListener('click', function (e) {
+                e.preventDefault();
+                changeActive(i);
+            })
+        }
+    }
 
-	if (select) {
-		select.addEventListener('change', function(e) {
-			let item = select.value;
-			changeActive(item);
-		})
-	}
+    if (select) {
+        select.addEventListener('change', function (e) {
+            let item = select.value;
+            changeActive(item);
+        })
+    }
 
-	function changeActive(n) {
-		for (let i = 0; i < priceBtn.length; i++) {
-			priceBtn[i].classList.remove('price-top-btn--active');
-			tab[i].classList.remove('price-bottom--active');
-		}
-		priceBtn[n].classList.add('price-top-btn--active');
-		tab[n].classList.add('price-bottom--active');
-	}
+    function changeActive(n) {
+        for (let i = 0; i < priceBtn.length; i++) {
+            materialsBtn[i].classList.remove('price-top-btn--active');
+            tab[i].classList.remove('price-bottom--active');
+        }
+        materialsBtn[n].classList.add('price-top-btn--active');
+        tab[n].classList.add('price-bottom--active');
+    }
 
 })();
+
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,2 +1,7 @@
 import "%modules%/header/header";
+import "%modules%/front-materials/front-materials";
+import "%modules%/slider-materials/slider-materials";
+
+
+
 import "%modules%/footer/footer"
