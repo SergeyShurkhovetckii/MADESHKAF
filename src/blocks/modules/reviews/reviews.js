fe667847212e14eaa82ada72dfd4f789403e8e3d
S19S93 <S19S93@yandex.ru>
2022-01-06 15:57:29 +0200

    Master

--- src/blocks/modules/reviews/reviews.js
+++ src/blocks/modules/reviews/reviews.js
@@ -1,12 +1,13 @@
+// Слайдер отзывы
 import Swiper_3 from 'swiper';
 
 var swiper = new Swiper_3(".mySwiper", {
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-      nextEl: ".rewiews-next",
-      prevEl: ".rewiews-prev",
+        nextEl: ".rewiews-next",
+        prevEl: ".rewiews-prev",
     },
-  });
+});
