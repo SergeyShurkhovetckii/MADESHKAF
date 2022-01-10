bca4766a1acfaf504cb2772e10e667f7b6df37c5
S19S93 <S19S93@yandex.ru>
2022-01-10 16:50:53 +0200

    Master

--- src/blocks/modules/reviews/reviews.js
+++ src/blocks/modules/reviews/reviews.js
@@ -1,10 +1,14 @@
 // Слайдер отзывы
 import Swiper_3 from 'swiper';
 
-var swiper = new Swiper_3(".mySwiper", {
+var swiper_3 = new Swiper_3(".rewiews-Swiper", {
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+        el: ".rewiews-pagination__pagination",
+        bulletClass:'bullets',
+        bulletActiveClass:'bullets--active',
+        clickable:true,
+        
+        
     },
     navigation: {
         nextEl: ".rewiews-next",
