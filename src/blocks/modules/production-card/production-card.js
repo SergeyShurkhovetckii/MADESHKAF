19ad3f12a31c6072b5cd1545619b456e0e760fab
S19S93 <S19S93@yandex.ru>
2022-01-11 17:45:00 +0200

    Master

---
+++ src/blocks/modules/production-card/production-card.js
@@ -0,0 +1,13 @@
+modules.define('production-card', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
