5fc8a198e5a2d6666c7b7a210e4c337f1e726e18
S19S93 <S19S93@yandex.ru>
2022-01-12 13:34:02 +0200

    Master

---
+++ src/blocks/modules/front-price-2/front-price-2.js
@@ -0,0 +1,13 @@
+modules.define('front-price-2', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
