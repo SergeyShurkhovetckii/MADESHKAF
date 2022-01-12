cb1a2fd4de64ff7fef450d909540f8b9b20dd43d
S19S93 <S19S93@yandex.ru>
2022-01-12 12:29:15 +0200

    Master

---
+++ src/blocks/modules/front-work/front-work.js
@@ -0,0 +1,13 @@
+modules.define('front-work', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
